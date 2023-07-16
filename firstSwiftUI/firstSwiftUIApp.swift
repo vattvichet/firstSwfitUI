d66e3ec7a4eb38625310b48ee734527c1e4df627
vichet vatt <vichetvattedu@gmail.com>
2023-07-16 14:23:46 +0700

    Initial Commit

---
+++ firstSwiftUI/firstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftUIApp.swift
+//  firstSwiftUI
+//
+//  Created by vichet vatt on 16/7/23.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
